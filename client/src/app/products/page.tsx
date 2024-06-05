8e37aca97ccb77aaa33439365f0a9d3e6cf9e57a
Romany <romanyfayiez@hotmail.com>
2024-06-05 16:37:47 +0100

    add dynamic product route

---
+++ client/src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+
+function ProductsPage() {
+  return (
+    <div>
+      <Link href='/products/1'>View design page</Link>
+    </div>
+  );
+}
+
+export default ProductsPage;
