faf8beef4489b86ce28d8a9c9ffc70177ca7f3c2
Romany <romanyfayiez@hotmail.com>
2024-06-05 17:04:35 +0100

    add image component

---
+++ client/src/components/product/image/ProductImg.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import React, { ReactElement } from 'react';
+
+type ProductImageProps = {
+  imageUrl: string;
+  alt?: string;
+};
+
+function ProductImage({ imageUrl, alt }: ProductImageProps): ReactElement {
+  return (
+    <Image
+      src={imageUrl}
+      width={300}
+      height={300}
+      alt={alt ?? 'product image'}
+      priority
+      quality={90}
+    />
+  );
+}
+
+export default ProductImage;
