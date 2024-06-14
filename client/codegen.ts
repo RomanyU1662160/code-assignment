37e980bb1cf395d2a322a3fc831bf4498017011d
Romany <romanyfayiez@hotmail.com>
2024-06-14 15:07:57 +0100

    implement codegen

---
+++ client/codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'http://localhost:3001',
+  documents: 'src/**/*.ts',
+  generates: {
+    'src/__generated__/': {
+      preset: 'client',
+      plugins: [],
+    },
+  },
+};
+
+export default config;
